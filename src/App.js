0a5b7eb73ecafb18fba9e938af9a4c714e040c75
Aaditya Pandey <aprock3004@gmail.com>
2024-01-15 18:48:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React,{ useState } from 'react';
+import Header  from './components/Header.js';
+import Body from './components/Body.js';
+import Footer from './components/Footer.js';
+import About from './components/About.js';
+import ContactUs from './components/ContactUs.js';
+import Shop from './components/Shop.js';
+import ProductPage from './components/ProductPage.js';
+import Login from './components/Login.js';
+import Signup from './components/Signup.js';
+import Cart from './components/Cart.js';
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
+
+const App = () => {
+
+  const [cartItems, setCartItems] = useState([]);
+  const addToCart = (item) => {
+    setCartItems((prevItems) => [...prevItems, { ...item, id: Date.now() }]);
+  };
+  const removeFromCart = (itemId) => {
+    setCartItems((prevItems) => prevItems.filter((item) => item.id !== itemId));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Header />
+        <Routes>
+            <Route path="/" element={<Body/>} />
+            <Route path="/contact" element={<ContactUs />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/shop" element={<Shop/>} />
+            <Route path="/product/:productId" element={<ProductPage addToCart={addToCart} />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/cart" element={<Cart cartItems={cartItems} removeFromCart={removeFromCart} />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
+
 
 export default App;
